7d1834356e21b5c132473fb112d1e9da1fbd8d26
ShrutheeshIR <shrutheesh99@gmail.com>
2020-05-12 11:25:19 +0530

    Changed transformation

--- src/tf2_pose/src/nodes/image_tf_read_rosbag.py
+++ src/tf2_pose/src/nodes/image_tf_read_rosbag.py
@@ -19,7 +19,7 @@ def main():
     """
     rotpc = np.array([[1,0,0], [0,-1,0], [0,0,-1]])
     args = {}
-    bag_file = '/home/olorin/Desktop/IISc/ROS-ARM-Planning/2020-05-11-14-47-40.bag'
+    bag_file = '/home/olorin/Desktop/IISc/ROS-ARM-Planning/2020-05-12-01-40-05.bag'
     depth_image_topic = '/camera/depth/image_raw'
     color_image_topic = '/camera/color/image_raw'
     tftopic = '/cameraposetransform'
@@ -32,7 +32,7 @@ def main():
     print(bag.get_start_time())
     print(bag.get_end_time())
     tf_t = tf.Transformer(True, rospy.Duration(360000.0))
-    mychangeR = np.array([[0,0,1], [-1,0,0], [0,1,0]])
+    mychangeR = np.array([[0,-1,0], [0,0,1], [1,0,0]])
     # hjhg
     bridge = CvBridge()
     print("ASJH")
@@ -45,14 +45,14 @@ def main():
             # print(msg)
             r = R.from_quat([msg.rotation.x, msg.rotation.y, msg.rotation.z, msg.rotation.w])
             # print(r.as_dcm())
-            # print*mychangeR
             
-            transmatrix[:3, :3] = np.dot(r.as_dcm(),mychangeR)
-            # print()
+            # transmatrix[:3, :3] = np.dot(mychangeR, r.as_dcm())
+            transmatrix[:3, :3] = r.as_dcm()
+            # print(transmatrix)
             # transmatrix[:3, :3] =np.eye(3,3)
-            transmatrix[2,3] = msg.translation.x
-            transmatrix[0,3] = -msg.translation.y
-            transmatrix[1,3] = msg.translation.z
+            transmatrix[0,3] = msg.translation.x
+            transmatrix[1,3] = msg.translation.y
+            transmatrix[2,3] = msg.translation.z
             # print(transmatrix)
             mbsst = r.as_rotvec()
 
@@ -63,33 +63,36 @@ def main():
 
             np.save('/home/olorin/Desktop/depthimgs/%06d.npy'%(count), cv_img)
             np.savetxt('/home/olorin/Desktop/depthimgs/%06d.txt'%(count), transmatrix)
-            if(count<30 and count>26):
-                cv2.imshow('IMAG', cv_img[400:600, 600:900]/1000 )
+            if(count%10==0 and count>75):
+                cv2.imshow('IMAG', cv_img/1000 )
                 cv2.waitKey(0)
                 cv2.destroyAllWindows()
                 dept = cv2.rgbd.depthTo3d(cv_img/1000,cam_intr)
-                print(transmatrix)
+                # print(transmatrix)
+                print(r.as_euler('xyz'))
                 r = R.from_dcm(np.linalg.inv(transmatrix[:3, :3]))
                 # print(dept.shape)
                 newdept = np.reshape(dept, (720*1280,3))
                 indicex = np.isnan(newdept).any(axis=1)
                 pcpnts = newdept[indicex==False]
                 # print(newdept)
+                r2 = R.from_dcm(mychangeR)
                 newdept2 = r.apply(pcpnts)
                 # print(newdept2)
                 newdept2 = newdept2-transmatrix[:3,3]
+                # newdept2 = r2.apply(newdept2)
                 # newdept2 = newdept[..., np.newaxis]
                 point_cloud = o3d.geometry.PointCloud()
                 point_cloud.points = o3d.utility.Vector3dVector(newdept2)
                 # print(transmatrix)
-                # point_cloud.rotate(rotpc)
+                point_cloud.rotate(rotpc)
                 # print(np.asarray(point_cloud.points))
                 # print(transmatrix[:3,:3])
                 # point_cloud.rotate(transmatrix[:3,:3])
                 # point_cloud.translate(transmatrix[:3,3])
                 # point_cloud.transform(transmatrix)
                 allpcs.append(point_cloud)
-                o3d.visualization.draw_geometries(allpcs)
+                # o3d.visualization.draw_geometries(allpcs)
 
                 
             # print(transmatrix)
